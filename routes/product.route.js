68285382dae789c3cb171416f23f0779370ead69
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2022-11-27 09:19:19 +0700

    rated product

--- routes/product.route.js
+++ routes/product.route.js
@@ -62,7 +62,7 @@ router.post("/", upload.array("hinhAnh"), async (req, res) => {
   const mm = String(today.getMonth() + 1).padStart(2, "0");
   const yyyy = today.getFullYear();
 
-  item.coBan.tinhTrang = "off"
+  item.coBan.tinhTrang = "off";
   item.coBan.soLuong = 0;
   item.coBan.ngayNhap = `${yyyy}-${mm}-${dd}`;
 
@@ -83,6 +83,14 @@ router.post("/", upload.array("hinhAnh"), async (req, res) => {
   });
 
   item.coBan.hinhAnh = await Promise.all(promises);
+
+  await rt
+    .ref(`${racmtName}/${key}/rated`)
+    .set({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
 
   await rt
     .ref(`${productName}/${key}`)
@@ -145,15 +153,15 @@ router.post("/racmt/:id", jsonParser, async (req, res) => {
 router.put("/:id/quantity", jsonParser, async (req, res) => {
   const { id } = req.params;
   const { quantity } = req.body;
-  
+
   await rt
-  .ref(`${productName}/${id}/coBan`)
-  .update({ soLuong: ServerValue.increment(quantity) })
-  .then((_) => res.sendStatus(200))
-  .catch((error) => {
-    console.error(error);
-    res.sendStatus(500);
-  });
+    .ref(`${productName}/${id}/coBan`)
+    .update({ soLuong: ServerValue.increment(quantity) })
+    .then((_) => res.sendStatus(200))
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
 });
 
 router.put("/:id/state", jsonParser, async (req, res) => {
@@ -161,13 +169,13 @@ router.put("/:id/state", jsonParser, async (req, res) => {
   const { state } = req.body;
 
   await rt
-  .ref(`${productName}/${id}/coBan`)
-  .update({ tinhTrang: state })
-  .then((_) => res.sendStatus(200))
-  .catch((error) => {
-    console.error(error);
-    res.sendStatus(500);
-  });
+    .ref(`${productName}/${id}/coBan`)
+    .update({ tinhTrang: state })
+    .then((_) => res.sendStatus(200))
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
 });
 
 export default router;
